7b37cf8e49ef178403ca73911efba1de97826824
sergeichestakov <me@sergei.com>
2023-03-16 14:39:49 -0400

    Set window.isDesktopApp in renderer context

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,4 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+import { contextBridge } from "electron";
+
+// Set `window.isDesktopApp`
+contextBridge.exposeInMainWorld('isDesktopApp', true);
