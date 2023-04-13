8d13935b0909c34a130538f8f6912380cb711d1a
sergeichestakov <me@sergei.com>
2023-04-13 16:44:10 -0700

    Remove useless comment

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,3 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
