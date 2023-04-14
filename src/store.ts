f54601f7a497dcae9e43f0f03abd7d8cd674cd88
sergeichestakov <me@sergei.com>
2023-04-14 14:12:18 -0700

    Handle missing key

--- src/store.ts
+++ src/store.ts
@@ -13,14 +13,26 @@ function createStore() {
     setLastSeenBackgroundColor(color: string) {
       store.set(keys.LAST_SEEN_BACKGROUND_COLOR, color);
     },
-    getLastSeenBackgroundColor(): string {
-      return store.get(keys.LAST_SEEN_BACKGROUND_COLOR) as string;
+    getLastSeenBackgroundColor(): string | null {
+      const bgColor = store.get(keys.LAST_SEEN_BACKGROUND_COLOR);
+
+      if (!bgColor) {
+        return null;
+      }
+
+      return bgColor as string;
     },
     setBounds(bounds: Rectangle) {
       store.set(keys.BOUNDS, bounds);
     },
-    getBounds(): Rectangle {
-      return store.get(keys.BOUNDS) as Rectangle;
+    getBounds(): Rectangle | null {
+      const bounds = store.get(keys.BOUNDS);
+
+      if (!bounds) {
+        return null;
+      }
+
+      return bounds as Rectangle;
     },
   };
 }
