01ca435016c930e7efa9218371b9acd0859f3690
sergeichestakov <me@sergei.com>
2023-05-02 15:19:27 -0400

    Add pull request template

---
+++ .github/pull_request_template.ts
@@ -0,0 +1,21 @@
+# Why
+
+<!-- Describe what prompted you to make this change, link relevant resources:
+
+- Asana task
+- Bug report
+- Design docs
+- Slack discussions
+- Figma link
+
+...etc -->
+
+# What changed
+
+<!-- Describe what changed to a level of detail that someone with no context with your PR could be able to review it -->
+
+<!-- Put screenshots/recordings of the change if relevant -->
+
+# Test plan 
+
+<!-- Describe what you did to test this change to a level of detail that allows your reviewer to test it -->
