d6af07bd0fa65c8c09e2b86ef8bc0b181248375a
sergeichestakov <me@sergei.com>
2023-08-08 10:59:29 -0400

    Add showMessageBox API

--- src/preload.ts
+++ src/preload.ts
@@ -20,6 +20,17 @@ function makeEventHandler(event: events) {
   };
 }
 
+// See: https://www.electronjs.org/docs/latest/api/dialog#dialogshowmessageboxbrowserwindow-options
+interface ShowMessageBoxParams {
+  message: string;
+  title?: string;
+  detail?: string;
+  defaultId?: number;
+  cancelId?: number;
+  buttons: Array<string>;
+  type?: "none" | "info" | "error" | "question" | "warning";
+}
+
 contextBridge.exposeInMainWorld("replitDesktop", {
   closeCurrentWindow: () => ipcRenderer.send(events.CLOSE_CURRENT_WINDOW),
   openReplWindow: (replSlug: string) =>
@@ -37,6 +48,8 @@ contextBridge.exposeInMainWorld("replitDesktop", {
       ipcRenderer.removeListener(events.AUTH_TOKEN_RECEIVED, listener);
     };
   },
+  showMessageBox: async (params: ShowMessageBoxParams) =>
+    ipcRenderer.invoke(events.SHOW_MESSAGE_BOX, params),
   onEnterFullscreen: makeEventHandler(events.ON_ENTER_FULLSCREEN),
   onLeaveFullscreen: makeEventHandler(events.ON_LEAVE_FULLSCREEN),
   logout: () => ipcRenderer.send(events.LOGOUT),
