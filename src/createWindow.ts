dc863aa865439765e9ce5674e39a4ff0e1ad89bc
Szymon Kaliski <kaliskiszymon@gmail.com>
2023-08-15 18:01:17 +0200

    [WS-689] minimize white flashes (#101)
    * minimize white flashes
    
    * `loadURL` after we set the event listeners on `window`
    
    * whitespace

--- src/createWindow.ts
+++ src/createWindow.ts
@@ -108,12 +108,13 @@ export function createWindow(props?: WindowProps): BrowserWindow {
     minHeight: 420,
     backgroundColor,
     autoHideMenuBar: true, // Window & Linux only, hides the menubar unless `Alt` is held
+    show: false, // We're starting with the window hidden, as we are still setting it up using imperative methods below
     ...platformStyling,
   });
 
   // Add a custom string to user agent to make it easier to differentiate requests from desktop app
   window.webContents.setUserAgent(
-    `${window.webContents.getUserAgent()} ReplitDesktop`
+    `${window.webContents.getUserAgent()} ReplitDesktop`,
   );
 
   // Prevent any URLs opened via a target="_blank" anchor tag or programmatically using `window.open` from
@@ -143,7 +144,7 @@ export function createWindow(props?: WindowProps): BrowserWindow {
   });
 
   // If the previous bounds are no longer in-bounds with the current set of
-  // displays, then bail and fallback to the defaul behavior to prevent the app
+  // displays, then bail and fallback to the default behavior to prevent the app
   // from opening stretched out or sticking outside the screens.
   const inBounds = isInBounds(store.getWindowBounds());
 
@@ -156,7 +157,7 @@ export function createWindow(props?: WindowProps): BrowserWindow {
   window.on("close", async () => {
     // We're capturing the background color to use as main browser window background color.
     const backgroundColor = await window.webContents.executeJavaScript(
-      `getComputedStyle(document.body).getPropertyValue('--background-root');`
+      `getComputedStyle(document.body).getPropertyValue('--background-root');`,
     );
     store.setLastSeenBackgroundColor(backgroundColor);
     store.setWindowBounds(window.getBounds());
@@ -179,6 +180,9 @@ export function createWindow(props?: WindowProps): BrowserWindow {
   // in order to ensure that we load the latest web build.
   // See: https://github.com/electron/electron/issues/1360#issuecomment-156506130
   window.loadURL(url, { extraHeaders: "pragma: no-cache\n" });
+
+  // We've set up the window, so let's show it!
+  window.show();
 
   return window;
 }
