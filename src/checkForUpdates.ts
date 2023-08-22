ce36c0e03403e343034d7a2b7bfe0b8e7d0f9cf4
Sergei Chestakov <me@sergei.com>
2023-08-22 13:45:43 -0400

    Check for updates on an interval, add "Later" button back (#110)
    * Check for updates on an interval, add "Later" button back
    
    * Set max check limit
    
    * Exponential backoff with a limit
    
    * start at 0
    
    * Set limit to 1 day
    
    * Add logs back
    
    * Reorder
    
    * Log button text directly
    
    * Better logging

--- src/checkForUpdates.ts
+++ src/checkForUpdates.ts
@@ -13,11 +13,15 @@ const url = `${server}/update/${platform}/${app.getVersion()}`;
 export default function checkForUpdates(): void {
   // The app must be packaged in order to check for updates.
   if (!isProduction) {
+    log.info('Skipping auto-update. App is not packaged.');
+
     return;
   }
 
   // The autoUpdater module does not support Linux
   if (isLinux()) {
+    log.info('Skipping auto-update. Auto-update is not supported on Linux.');
+
     return;
   }
 
@@ -30,23 +34,32 @@ export default function checkForUpdates(): void {
       e,
     );
 
+    Sentry.captureException(e);
+
     return;
   }
 
+  let timeout: NodeJS.Timer = null;
   autoUpdater.on('update-downloaded', () => {
     const dialogOpts = {
       type: 'info' as const,
-      buttons: ['Restart'],
+      buttons: ['Restart', 'Later'],
       title: 'Application Update',
       message: 'New Update Available',
       detail:
         'A new version has been downloaded. Restart the application to apply the updates.',
     };
 
+    // We downloaded an update so it's safe to stop trying because the user will either accept
+    // and reload or defer the update until later in which case we should not show it again.
+    clearTimeout(timeout);
     log.info('Update downloaded');
 
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      log.info('Update dialog selected: ', returnValue);
+      log.info(
+        'Update dialog selected: ',
+        dialogOpts.buttons[returnValue.response],
+      );
 
       if (returnValue.response === 0) {
         autoUpdater.quitAndInstall();
@@ -54,19 +67,31 @@ export default function checkForUpdates(): void {
     });
   });
 
-  autoUpdater.on('error', (message) => {
-    log.error('There was a problem updating the application');
-    log.error(message);
+  autoUpdater.on('error', (error) => {
+    log.error('There was a problem updating the application: ', error.message);
 
-    const error = new Error('Failed to auto-update the application');
-
-    Sentry.captureException(error, {
-      extra: {
-        message,
-      },
-    });
+    Sentry.captureException(error);
   });
 
-  log.info('Checking for updates');
-  autoUpdater.checkForUpdates();
+  const thirtyMinInMs = 30 * 60 * 1000;
+  const oneDayInMs = 24 * 60 * 60 * 1000;
+  function tryCheckForUpdates() {
+    log.info('Checking for updates');
+    autoUpdater.checkForUpdates();
+  }
+
+  function scheduleCheckForUpdates(attempt = 1) {
+    timeout = setTimeout(
+      () => {
+        tryCheckForUpdates();
+        scheduleCheckForUpdates(Math.min(attempt + 1, 10));
+      },
+      // exponential backoff from 1/2 hour until we reach 24 hours
+      Math.max(thirtyMinInMs * 2 ** (attempt - 1), oneDayInMs),
+    );
+  }
+
+  // Check for updates once on launch and then keep retrying
+  tryCheckForUpdates();
+  scheduleCheckForUpdates();
 }
