4d552ac7f5994f3db80a4d38aa35e18a41172cf5
sergeichestakov <me@sergei.com>
2023-09-29 11:54:31 -0700

    Manually invoke Sentry crash reporter

--- src/sentry.ts
+++ src/sentry.ts
@@ -3,10 +3,15 @@ import { isProduction } from './constants';
 import log from 'electron-log/main';
 import store from './store';
 import type { User } from './types';
+import { crashReporter } from 'electron';
 
 // DSN for "desktop" project in Sentry
 const dsn =
   'https://d88a656213ca4c1892091cc955fd7783@o1151714.ingest.sentry.io/4505167464693760';
+
+// See https://docs.sentry.io/platforms/javascript/guides/electron/#electron-native-manual
+const submitURL =
+  'https://o1151714.ingest.sentry.io/api/4505167464693760/minidump/?sentry_key=d88a656213ca4c1892091cc955fd7783';
 
 export function initSentry(): void {
   if (!isProduction) {
@@ -18,8 +23,11 @@ export function initSentry(): void {
   Sentry.init({
     dsn,
   });
-
   setSentryUser(store.getUser());
+
+  crashReporter.start({
+    submitURL,
+  });
 }
 
 export function setSentryUser(user: User | null) {
